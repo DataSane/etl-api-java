d7a6e5b1800104a0f8d5a7a90fc4be04f5d65d93
kauansfr <kauan.franca@sptech.school>
2024-10-05 16:39:08 -0300

    Inserção de Logs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import apache_configuration.GerenciadorMunicipio;
 import apache_configuration.Municipio;
 import com.github.javafaker.Faker;
+import logs_config.LogHandler;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -13,11 +14,14 @@ public class Main {
         Conexao conexao = new Conexao();
         JdbcTemplate con = conexao.getConexao();
         GerenciadorMunicipio gerenciadorMunicipio = new GerenciadorMunicipio();
+        LogHandler mainLogger = new LogHandler(); // intancia, pra usar o método
 
+        mainLogger.setLog(3,"DROP, CREATE e USE database", Main.class.getName());
         con.execute("DROP DATABASE datasaneBD");
         con.execute("CREATE DATABASE datasaneBD");
         con.execute("USE datasaneBD");
 
+        mainLogger.setLog(3,"DROP e CREATE tabela Municipios", Main.class.getName());
         con.execute("DROP TABLE IF EXISTS Municipios");
         con.execute("""
                 CREATE TABLE Municipios(
@@ -35,12 +39,12 @@ public class Main {
 
         try {
             listaMunicipios = gerenciadorMunicipio.criar();
+            mainLogger.setLog(3,"Células lidas com sucesso", Main.class.getName());
         }catch (IOException ex){
-            System.out.println("ERRO:" + ex.getMessage());
+            mainLogger.setLog(1,ex.getMessage(), Main.class.getName());
         }
 
-
-
+        mainLogger.setLog(3,"Iniciando inserção dos objetos no Banco", Main.class.getName());
 
         for (Municipio municipio : listaMunicipios) {
             con.update("INSERT INTO Municipios VALUES (DEFAULT, ?, ?, ?, ?, ?, ?, ?)", municipio.getMunicipio(), municipio.getPopulacao(), municipio.getPopulacaoSemColetaDeLixo(),
@@ -48,8 +52,12 @@ public class Main {
             );
         }
 
+        mainLogger.setLog(3,"Todos objetos inseridos no Banco", Main.class.getName());
+
+
         List<Municipios> municipios = con.query("SELECT * FROM Municipios", new BeanPropertyRowMapper<>(Municipios.class));
 
+        mainLogger.setLog(3,"Exibindo dados da tabela MySQL", Main.class.getName());
         for (Municipios municipio : municipios) {
             System.out.println("""
                     idMunicipios: %d
@@ -63,5 +71,6 @@ public class Main {
                     """.formatted(municipio.getIdMunicipios(), municipio.getNome(), municipio.getPopulacaoTotal(), municipio.getPopulacaoSemLixo(),
                     municipio.getPopulacaoSemAgua(), municipio.getPopulacaoSemEsgoto(), municipio.getDomicilioSujeitoInundacoes(), municipio.getPossuiPlanoMunicipal()));
         }
+        mainLogger.setLog(3,"Exibição de objetos finalizada", Main.class.getName());
     };
 }
