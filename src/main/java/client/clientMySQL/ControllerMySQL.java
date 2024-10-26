9498183a525fb07f6b3b16d1c9bf1f65654dca74
kauansfr <kauan.franca@sptech.school>
2024-10-25 23:35:19 -0300

    implementing-namedatabase-through-dotenv

--- src/main/java/client/clientMySQL/ControllerMySQL.java
+++ src/main/java/client/clientMySQL/ControllerMySQL.java
@@ -19,7 +19,7 @@ public class ControllerMySQL {
     LogHandler mainLogger = new LogHandler(); // intancia, pra usar o método
 
     Boolean porcentagemAplicada = true;
-    String nameDatabase = "datasaneTESTE";
+    String nameDatabase = conexao.getNameDatabase();
     String nameTable = null;
 
     List<Municipio> listaMunicipios = new ArrayList<>();
